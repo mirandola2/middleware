96d54aa0532f18cc0586c6360123223fe117bd5d
Giacomo Salici <giambo2000@gmail.com>
2023-09-09 23:48:49 +0200

    birthdays support

--- src/worker.ts
+++ src/worker.ts
@@ -10,7 +10,7 @@ export default {
 
 		const queryParams: { [key: string]: string } = {};
 		for (const [key, value] of searchParams.entries()) {
-		queryParams[key] = value;
+			queryParams[key] = value;
 		}
 
 		const corsHeaders = {
@@ -27,17 +27,27 @@ export default {
 
 		if (pathname === '/data') {
 			// If you did not use `DB` as your binding name, change it here
-			const fiscal_code = queryParams['id']
+			const fiscal_code = queryParams['id'];
 			const { results } = await env.DB.prepare('SELECT member_code FROM data WHERE fiscal_code=?').bind(fiscal_code).all();
-			var listOfCodes:String[] = [];
-			results.forEach(element => {
-				listOfCodes.push(String(element?.member_code))
+			var listOfCodes: String[] = [];
+			results.forEach((element) => {
+				listOfCodes.push(String(element?.member_code));
 			});
 			return new Response(JSON.stringify(listOfCodes), { headers: corsHeaders });
 		}
 
-		return new Response('Call /totem to retrieve all the totems. \nCall /data?id=XYZ to obtain the member code of the person whose fiscal code corresponds to XYZ.');
+		if (pathname === '/birthday') {
+			// If you did not use `DB` as your binding name, change it here
+
+			const { results } = await env.DB.prepare(
+				"SELECT full_name as name, strftime('%j', birthday) - strftime('%j', 'now') AS t_minus FROM data WHERE strftime('%m-%d', birthday) >= strftime('%m-%d', 'now') AND strftime('%m-%d', birthday) <= strftime('%m-%d', 'now', '+7 days') ORDER BY strftime('%m-%d', birthday) ASC, full_name;"
+			).all();
+
+			return new Response(JSON.stringify(results), { headers: corsHeaders });
+		}
+
+		return new Response(
+			'Call /totem to retrieve all the totems. \nCall /data?id=XYZ to obtain the member code of the person whose fiscal code corresponds to XYZ.\nCall /birthday to get the names of the people that have a birthday in the current week and the days until that date (t-minus).'
+		);
 	},
 };
-
-
