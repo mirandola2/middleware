d9f4e6366c704c2c07bdc46b102bcea61367eb0e
Giacomo Salici <giambo2000@gmail.com>
2023-09-09 20:15:55 +0200

    fiscal code binding

--- src/worker.ts
+++ src/worker.ts
@@ -2,42 +2,40 @@ export interface Env {
 	// If you set another name in wrangler.toml as the value for 'binding',
 	// replace "DB" with the variable name you defined.
 	DB: D1Database;
-  }
-  
-  export default {
+}
+
+export default {
 	async fetch(request: Request, env: Env) {
-	  const { pathname } = new URL(request.url);
+		const { pathname } = new URL(request.url);
 
-	  const corsHeaders = {
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Methods": "GET, OPTIONS",
-		"Access-Control-Allow-Headers": "*",
-	  }
-	  
-  
-	  if (pathname === "/totem") {
-		// If you did not use `DB` as your binding name, change it here
-		const { results } = await env.DB.prepare(
-		  "SELECT name, totem, location, year FROM totem order by year, name"
-		)
-		  .all();
-		return new Response(JSON.stringify(results), {headers: corsHeaders})
-		
-		
-	  }
-  
-	  return new Response(
-		"Call /totem to get all the totems"
-	  );
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, OPTIONS',
+			'Access-Control-Allow-Headers': '*',
+		};
+
+		if (pathname === '/totem') {
+			// If you did not use `DB` as your binding name, change it here
+			const { results } = await env.DB.prepare('SELECT name, totem, location, year FROM totem order by year, name').all();
+			return new Response(JSON.stringify(results), { headers: corsHeaders });
+		}
+
+		if (pathname === '/data') {
+			// If you did not use `DB` as your binding name, change it here
+			const fiscal_code = getQueryParameters(new URL(request.url))['id']
+			const { results } = await env.DB.prepare('SELECT member_code FROM data WHERE fiscal_code=?').bind(fiscal_code).all();
+			return new Response(JSON.stringify(results), { headers: corsHeaders });
+		}
+
+		return new Response('Call /totem to retrieve all the totems. \nCall /data?id=XYZ to obtain the member code of the person whose fiscal code corresponds to XYZ.');
 	},
-  };
+};
 
 
-
-
-
-
-
-
-
-  
+function getQueryParameters(url: URL): { [key: string]: string } {
+	const queryParams: { [key: string]: string } = {};
+	for (const [key, value] of url.searchParams.entries()) {
+	  queryParams[key] = value;
+	}
+	return queryParams;
+  }
