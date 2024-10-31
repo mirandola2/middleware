00e5c3a0891492f3b60544a394c497125ff7244f
Giacomo Salici <giambo2000@gmail.com>
2024-10-31 18:25:39 +0100

    bd better not solved

--- src/worker.ts
+++ src/worker.ts
@@ -35,15 +35,26 @@ export default {
 		if (pathname === '/birthday') {
 			// If you did not use `DB` as your binding name, change it here
 
+			const dayRange = Number(queryParams["t"]) || 7
 			const { results } = await env.DB.prepare(
-				"SELECT full_name as name, strftime('%m-%d', birthday) as bd, strftime('%j', birthday) - strftime('%j', 'now') AS t_minus FROM data WHERE strftime('%m-%d', birthday) >= strftime('%m-%d', 'now', 'localtime' ) AND strftime('%m-%d', birthday) <= strftime('%m-%d', 'now', 'localtime', '+7 days') ORDER BY strftime('%m-%d', birthday) ASC, full_name;"
-			).all();
-
+				`SELECT full_name as name,
+					strftime('%m-%d', birthday, 'localtime') as bd,
+					CASE 
+						WHEN (strftime('%j', birthday, 'localtime') - strftime('%j', 'now', 'localtime')) < 0 
+						THEN (strftime('%j', birthday, 'localtime') - strftime('%j', 'now', 'localtime')) + 365
+						ELSE (strftime('%j', birthday, 'localtime') - strftime('%j', 'now', 'localtime'))
+					END AS t_minus 
+				FROM data 
+				WHERE t_minus BETWEEN 0 AND ?
+				ORDER BY t_minus ASC, full_name;`
+			).bind(dayRange).all();
+						
+			
 			return new Response(JSON.stringify(results), { headers: corsHeaders });
 		}
 
 		return new Response(
-			'Call /totem to retrieve all the totems. \nCall /data?name=XYZ to obtain the member code of the person whose full name corresponds to XYZ (uri encoded).\nCall /birthday to get the names of the people that have a birthday in the current week and the days until that date (t-minus).'
+			'Call /totem to retrieve all the totems. \nCall /data?name=XYZ to obtain the member code of the person whose full name corresponds to XYZ (uri encoded).\nCall /birthday?t=7 to get the names of the people that have a birthday in the current week and the days until that date (t-minus, default 7).'
 		);
 	},
 };
