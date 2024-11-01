8f213ca08571f4aeb46eb0fa2abb85f702eefa8e
Giacomo Salici <giambo2000@gmail.com>
2024-11-01 02:37:53 +0100

    better date

--- src/worker.ts
+++ src/worker.ts
@@ -33,35 +33,36 @@ export default {
 		}
 
 		if (pathname === '/birthday') {
-			// If you did not use `DB` as your binding name, change it here
+
+			
+			const currentYear = new Date().getFullYear();
+			const isLeapYear = new Date(currentYear, 1, 29).getDate() === 29;
+			const daysInYear = isLeapYear ? 366 : 365;
+			  
 
 			const dayRange = Number(queryParams["t"]) || 7
-						
-			// Get the timezone from Cloudflare
-			const timezone = "Europe/Rome"
-
-			// Create a localized date in the specified timezone
-			let localizedDate = new Date(
-				new Date().toLocaleString("en-US", { timeZone: timezone })
-			);
-			console.log(localizedDate)
-
-			// Get the Unix epoch time for the localized date
-			const localizedUnixEpoch = Math.floor(localizedDate.getTime() / 1000); // Convert to seconds
+			var day = Number(queryParams["d"])
+			if (Number.isNaN(day)){
+				var now = new Date();
+				var start = new Date(now.getFullYear(), 0, 0);
+				var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+				var oneDay = 1000 * 60 * 60 * 24;
+				day = Math.floor(diff / oneDay);
+			}
 
 			// Now use this Unix epoch in the SQL query
 			const { results } = await env.DB.prepare(
-				`SELECT full_name as name,
-					strftime('%m-%d', birthday) as bd,
-					CASE 
-						WHEN (strftime('%j', birthday) - strftime('%j', ?, 'unixepoch')) < 0 
-						THEN (strftime('%j', birthday) - strftime('%j', ?, 'unixepoch')) + 365
-						ELSE (strftime('%j', birthday) - strftime('%j', ?, 'unixepoch'))
-					END AS t_minus 
+				`SELECT full_name AS name,
+						strftime('%m-%d', birthday) AS bd,
+						CASE 
+							WHEN (strftime('%j', birthday) - ?) < 0 
+								THEN (strftime('%j', birthday) - ? + ?)
+							ELSE (strftime('%j', birthday) - ?)
+						END AS t_minus
 				FROM data 
 				WHERE t_minus BETWEEN 0 AND ?
 				ORDER BY t_minus ASC, full_name;`
-			).bind(localizedUnixEpoch, localizedUnixEpoch, localizedUnixEpoch, dayRange).all();
+			).bind(day, day, daysInYear, day, dayRange).all();
 
 									
 			
@@ -69,7 +70,7 @@ export default {
 		}
 
 		return new Response(
-			'Call /totem to retrieve all the totems. \nCall /data?name=XYZ to obtain the member code of the person whose full name corresponds to XYZ (uri encoded).\nCall /birthday?t=7 to get the names of the people that have a birthday in the current week and the days until that date (t-minus, default 7).'
+			'Call /totem to retrieve all the totems. \nCall /data?name=XYZ to obtain the member code of the person whose full name corresponds to XYZ (uri encoded).\nCall /birthday?t=7&d=306 to get the names of the people that have a birthday in the `t` days following the day `d` and the days until that date (t-minus, `t` default 7, `d` default today - please be aware of timezones, must be a day of the year [1:366]).'
 		);
 	},
 };
